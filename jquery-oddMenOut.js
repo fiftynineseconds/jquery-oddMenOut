892d104b1130cb9b9e2986c1720cb68f0f3810fa
fiftynineseconds <jason@catapultweb.com>
2016-03-22 14:51:17 -0400

    cleaning up code

--- jquery-oddMenOut.js
+++ jquery-oddMenOut.js
@@ -1,54 +1,57 @@
-﻿(function ($)
-{
-    $.fn.oddMenOut = function ()
-    {
-        return this.each(function ()
-        {
-            var $this = $(this);
+﻿(function ($) {
+	$.fn.oddMenOut = function ()
+	{
+		return this.each(function ()
+		{
+			var $this = $(this);
 
 			// get a list of each unique value as well as the number of times it's found.
-			
+
 			// get values
 			var cell_index = $this.find("thead tr").find("th.oddMenOut-highlight").index();
 			var row_tds = $("tr td:nth-child(" + (cell_index + 1) + ")")
-			var row_values = row_tds.map(function () {
+
+			var row_values = row_tds.map(function ()
+			{
 				return $(this).text();
 			}).get();
-			
+
 			// get unique values
 			var unique = [];
-			for (var i=0, l=row_values.length; i<l; i++)
+
+			for (var i = 0; i < row_values.length; i++)
+			{
+				// if not found and not empty then add to array
 				if (unique.indexOf(row_values[i]) === -1 && row_values[i] !== '')
 					unique.push(row_values[i]);
-				
+			}
+
 			// get times found for each unique value
 			var counts = new Array(unique.length);
-			
-			for(var x=0; x<unique.length; x++)
+
+			for(var i=0; i<unique.length; i++)
 			{
-				var currentval = unique[x];
-				
-				for (var l = row_values.length, c = 0; l--; )
+				var currentval = unique[i];
+				var currentcount = 0;
+
+				for(var j=0; j<row_values.length;j++)
 				{
-					if ( row_values[ l ] === currentval )
-					{
-						c++;
-					}
+					if(row_values[j] == currentval)
+						currentcount++;
 				}
-				
-				counts[x] = c;
+				counts[i] = currentcount++;
 			}
 
 			// find the value that has the highest number.
 			// todo: what if there is more than one highest number?
 			var highest_number = Math.max.apply(null, counts);
-			
+
 			// find the element in the unique array at the index
 			var elementPos = counts.indexOf(highest_number);
 			var highest_element = unique[elementPos];
-			
+
 			// highlight the rows that are not the value of the highest number.
 			$this.find("tr td:nth-child(" + (cell_index + 1) + "):not(:contains('" + highest_element + "'))").css('background-color', 'red');
-        });
-    };
+		});
+	};
 })(jQuery);
